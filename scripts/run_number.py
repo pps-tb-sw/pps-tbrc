d8805feb04e3e7afcc5d923b7ee9dbb9d616ad05
Laurent Forthomme <laurent.forthomme@uclouvain.be>
2015-09-18 14:16:12 +0200

    Added the starting datetime to the run information provided by ppsLastRun

--- scripts/run_number.py
+++ scripts/run_number.py
@@ -1,12 +1,15 @@
 import sqlite3
 import os
+from datetime import datetime
 
 def main():
   path = os.getenv('PPS_PATH', '/home/ppstb/pps-tbrc/build/')
   conn = sqlite3.connect(path+'/run_infos.db')
   c = conn.cursor()
-  c.execute('SELECT id FROM run ORDER BY id DESC LIMIT 1')
-  print 'Last Run number is:', c.fetchone()[0]
+  c.execute('SELECT id,start FROM run ORDER BY id DESC LIMIT 1')
+  run_id, start_ts = c.fetchone()
+  print 'Last Run number is:', run_id
+  print '  -- started on', datetime.fromtimestamp(int(start_ts))
 
 if __name__=='__main__':
   main()
