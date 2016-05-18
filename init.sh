8d1a133c00eecc9faf59773055af9d34e345983d
Laurent Forthomme <laurent.forthomme@uclouvain.be>
2016-05-18 16:16:44 +0200

    Modified the CMakeLists.txt to take into account the new dependencies between submodules

--- init.sh
+++ init.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
 git submodule update --init --recursive # fetch the latest version of xpedaq
+git submodule foreach git pull origin master
 cd external/xpedaq/quickusb-2.15.2/linux && source $PWD/setup.sh && qusb_link_libs
 cd -
